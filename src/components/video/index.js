05aad1cdd93cded487540816023e3b391b192109
Iago Rocha Porto <iagorochaporto@gmail.com>
2019-05-14 22:16:31 -0300

    primeiro uso do redux

---
+++ src/components/video/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Video extends React.Component {
+  render() {
+    return(
+      <div>
+        <strong>Modulo x</strong>
+        <span>Aula X</span>
+      </div>
+    )   
+  }
+}
